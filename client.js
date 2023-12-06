fb7759dedc36ba7dffd65a0215fbdfb94589c9c6
afrid <afridahamed14@gmail.com>
2023-12-06 16:21:34 +0530

    Modified files

--- client.js
+++ client.js
@@ -9,33 +9,35 @@ const session = http2.connect("http://localhost:8000");
 
 // ********** POST book(s) to the server **********
 
-// const postReq = session.request({ ":path": "/", ":method": "POST" });
-// postReq.write(
-//   JSON.stringify([
-//     { title: "Frankenstein", author: "Mary Shelley" },
-//     { title: "Demo", author: "Afrid" },
-//   ]),
-//   "utf8"
-// );
-// postReq.end();
+const postReq = session.request({ ":path": "/", ":method": "POST" });
+postReq.write(
+  JSON.stringify([
+    { title: "Frankenstein", author: "Mary Shelley" },
+    { title: "Demo", author: "Afrid" },
+  ]),
+  "utf8"
+);
+postReq.end();
 
-// postReq.on("response", (headers) => console.log(headers));
-// postReq.setEncoding("utf8");
-// postReq.on("data", (data) => console.log(data));
+postReq.on("response", (headers) => console.log(headers));
+postReq.setEncoding("utf8");
+postReq.on("data", (data) => console.log(data));
 
 // ********** GET book(s) based on the title **********
 
 // To be used only after the POST is done, so it would be commented for the time being since
 // the process is executed from the terminal
 
-const title = "Demo";
+// Will be uncommented after the POST operation
 
-const getReq = session.request({
-  ":path": `/?title=${title}`,
-  ":method": "GET",
-});
-getReq.end();
+// const title = "Demo";
 
-getReq.on("response", (headers) => console.log(headers));
-getReq.setEncoding("utf8");
-getReq.on("data", (data) => console.log(data));
+// const getReq = session.request({
+//   ":path": `/?title=${title}`,
+//   ":method": "GET",
+// });
+// getReq.end();
+
+// getReq.on("response", (headers) => console.log(headers));
+// getReq.setEncoding("utf8");
+// getReq.on("data", (data) => console.log(data));
